794ebb5b6e750d4cd27b09658f9de1266a997303
WanderQY <90610198+WanderQY@users.noreply.github.com>
2023-10-12 14:29:37 +0800

    Update path_corr.py

--- Codelist/Hierarchy with DataAug/path_corr.py
+++ Codelist/Hierarchy with DataAug/path_corr.py
@@ -6,22 +6,22 @@ def path_correction2(input_hier, predict_score):
         predict_score,(hier_num, num_spe)
         class_list = [SELECT_CLASS, SELECT_GENUS, SELECT_FAMILY, SELECT_ORDER]
     """
-    match_list = []  # 预测路径与每条正确路径的匹配节点, [[False, False, True, True],...]
-    match_score = []  # 预测路径与每条正确路径的匹配节点个数, [2,...]
+    match_list = []  # Match nodes between the predicted path and each correct path, [[False, False, True, True],...]
+    match_score = []  # The number of matching nodes between the predicted path and each correct path, [2,...]
     for t in tree:
         match_list.append([input_hier[0] == t[0], input_hier[1] == t[1], input_hier[2] == t[2], input_hier[3] == t[3]])
     for i in match_list:
         match_score.append(sum(i))
-    # 候选路径位置
+    # Candidate path location
     loc = [i for i in range(len(match_score)) if match_score[i] == max(match_score)]
 
-    if len(loc) == 1:  # 候选路径只有一条，则返回该路径
+    if len(loc) == 1:  # If there is only one candidate path, return the path
         return tree[loc[0]]
-    else:  # 候选路径多条，按底层匹配优先排序
+    else:  # If there are multiple candidate paths, resorted according to the finer level priority.
         loc_score = []
         for i in loc:
             list = match_list[i][::-1]
-            num = 0  # 节点匹配数量累计分数
+            num = 0  # Cumulative score of node matching quantity
             for j, k in enumerate(list):
                 if k:
                     num += j
@@ -30,8 +30,8 @@ def path_correction2(input_hier, predict_score):
         choose_loc = loc_score[0][0]
         corr_hier = tree[choose_loc]
 
-        path_match = match_list[choose_loc]  # 候选路径匹配， [False, False, True, True]
-        # 若匹配节点为叶子节点，则直接返回
+        path_match = match_list[choose_loc]  # candidate path matching, [False, False, True, True]
+        # If the matching node is a leaf node, it will be returned directly.
         k = 0
         while not path_match[k] and not path_match[k+1]:
             if k+1 == len(path_match):
